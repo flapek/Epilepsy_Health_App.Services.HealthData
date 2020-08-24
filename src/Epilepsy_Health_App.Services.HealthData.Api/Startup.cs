2e997cebf01db8afb93ebc0ac0f1f797b6d742e3
flapek <filap4@gmail.com>
2020-08-24 23:29:29 +0200

    clean project

--- src/Epilepsy_Health_App.Services.HealthData.Api/Startup.cs
+++ src/Epilepsy_Health_App.Services.HealthData.Api/Startup.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Text;
+using Convey;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
+using SCL.Auth;
 
 namespace Epilepsy_Health_App.Services.HealthData.Api
 {
@@ -21,31 +20,8 @@ namespace Epilepsy_Health_App.Services.HealthData.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var audienceConfig = Configuration.GetSection("Audience");
-
-            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(audienceConfig["Secret"]));
-            var tokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = signingKey,
-                ValidateIssuer = true,
-                ValidIssuer = audienceConfig["Iss"],
-                ValidateAudience = true,
-                ValidAudience = audienceConfig["Aud"],
-                ValidateLifetime = true,
-                ClockSkew = TimeSpan.Zero,
-                RequireExpirationTime = true,
-            };
-
-            services.AddAuthentication(o =>
-            {
-                o.DefaultAuthenticateScheme = "TestKey";
-            })
-            .AddJwtBearer("TestKey", x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.TokenValidationParameters = tokenValidationParameters;
-            });
+            services.AddConvey()
+                .AddJwt();
 
             services.AddControllers();
         }
