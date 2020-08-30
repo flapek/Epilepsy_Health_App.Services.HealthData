57f64404ec17015d1d4f6d1e48f73361cdf7b5c0
flapek <filap4@gmail.com>
2020-08-30 08:58:12 +0200

    clean project, and add new library

--- src/Epilepsy_Health_App.Services.HealthData.Api/Startup.cs
+++ src/Epilepsy_Health_App.Services.HealthData.Api/Startup.cs
@@ -1,10 +1,10 @@
-using Convey;
+using Joint;
+using Joint.Auth;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SCL.Auth;
 
 namespace Epilepsy_Health_App.Services.HealthData.Api
 {
@@ -20,7 +20,7 @@ namespace Epilepsy_Health_App.Services.HealthData.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddConvey()
+            services.AddJoint()
                 .AddJwt();
 
             services.AddControllers();
