33bf28f31cc0980b13e2191beed25ba3db6f26dc
Flapek <filap4@gmail.com>
2020-12-31 10:59:38 +0100

    implement claim controller method

--- src/Epilepsy_Health_App.Services.HealthData.Application/Controllers/IClaimsController.cs
+++ src/Epilepsy_Health_App.Services.HealthData.Application/Controllers/IClaimsController.cs
@@ -1,6 +1,9 @@
-﻿namespace Epilepsy_Health_App.Services.HealthData.Application.Controllers
+﻿using System;
+
+namespace Epilepsy_Health_App.Services.HealthData.Application.Controllers
 {
     public interface IClaimsController
     {
+        Guid GetUserId(string jwt);
     }
 }
--- src/Epilepsy_Health_App.Services.HealthData.Infrastructure/Controllers/ClaimsController.cs
+++ src/Epilepsy_Health_App.Services.HealthData.Infrastructure/Controllers/ClaimsController.cs
@@ -1,8 +1,21 @@
 ﻿using Epilepsy_Health_App.Services.HealthData.Application.Controllers;
+using Epilepsy_Health_App.Services.HealthData.Infrastructure.Exceptions;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 
 namespace Epilepsy_Health_App.Services.HealthData.Infrastructure.Controllers
 {
     internal sealed class ClaimsController : IClaimsController
     {
+        public Guid GetUserId(string jwt)
+        {
+            jwt = jwt.Remove(0, jwt.IndexOf(' ')).Trim();
+            return new JwtSecurityTokenHandler().ReadToken(jwt) as JwtSecurityToken is null
+                       ? throw new EmptyClaimsException()
+                       : !Guid.TryParse((new JwtSecurityTokenHandler().ReadToken(jwt) as JwtSecurityToken).Claims.First(claim => claim.Type == "sub").Value, out Guid result)
+                       ? throw new EmptyClaimsException()
+                       : result;
+        }
     }
 }
