63163454ab850438a377cd479347187dd0c0f242
Flapek <filap4@gmail.com>
2020-12-31 10:53:42 +0100

    code refactor

--- src/Epilepsy_Health_App.Services.HealthData.Api/Program.cs
+++ src/Epilepsy_Health_App.Services.HealthData.Api/Program.cs
@@ -6,10 +6,8 @@ namespace Epilepsy_Health_App.Services.HealthData.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) 
+            => CreateHostBuilder(args).Build().Run();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
