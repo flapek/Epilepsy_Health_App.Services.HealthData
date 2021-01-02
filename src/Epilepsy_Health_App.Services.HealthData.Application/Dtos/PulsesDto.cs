65c99eaa0aa0c73253a14e6e3aa31ca6bbddea38
Flapek <filap4@gmail.com>
2021-01-02 09:36:58 +0100

    remove unnecessary prop

--- src/Epilepsy_Health_App.Services.HealthData.Application/Dtos/PulsesDto.cs
+++ src/Epilepsy_Health_App.Services.HealthData.Application/Dtos/PulsesDto.cs
@@ -5,7 +5,6 @@ namespace Epilepsy_Health_App.Services.HealthData.Application.Dtos
 {
     public class PulsesDto
     {
-        public Guid UserId { get; set; }
         public DateTime From { get; set; }
         public DateTime To { get; set; }
         public IEnumerable<PulseDto> PulseDtos { get; set; }
