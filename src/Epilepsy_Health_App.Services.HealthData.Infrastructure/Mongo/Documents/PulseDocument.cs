80d7223ea4b52e36e2c8f047c16c520500dfa7af
Flapek <filap4@gmail.com>
2021-01-02 09:41:41 +0100

    add mongo document

---
+++ src/Epilepsy_Health_App.Services.HealthData.Infrastructure/Mongo/Documents/PulseDocument.cs
@@ -0,0 +1,13 @@
+﻿using Joint.Types;
+using System;
+
+namespace Epilepsy_Health_App.Services.HealthData.Infrastructure.Mongo.Documents
+{
+    public class PulseDocument : IIdentifiable<Guid>
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Value { get; set; }
+    }
+}
