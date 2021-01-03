67b63e2b89f22a340b5b23c7e38f4753a55bd388
flapek <filap4@gmail.com>
2021-01-03 10:46:58 +0100

    remove unnecessary property

--- src/Epilepsy_Health_App.Services.HealthData.Application/Dtos/PulsesDto.cs
+++ src/Epilepsy_Health_App.Services.HealthData.Application/Dtos/PulsesDto.cs
@@ -5,8 +5,6 @@ namespace Epilepsy_Health_App.Services.HealthData.Application.Dtos
 {
     public class PulsesDto
     {
-        public DateTime From { get; set; }
-        public DateTime To { get; set; }
         public IEnumerable<PulseDto> PulseDtos { get; set; }
     }
 }
